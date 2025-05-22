6bdeac9dc63cf6d9acd02efee47f3b785d768cf9
Mauro César <mauroguima080@hotmail.com>
2025-05-22 09:37:54 -0300

    realizando o deploy no render

--- app.py
+++ app.py
@@ -35,5 +35,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print("Iniciando servidor Flask...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000, debug=False)
