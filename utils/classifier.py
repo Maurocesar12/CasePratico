66806657d18ce57e643e68d6aa29efb0fff27c10
Mauro César <mauroguima080@hotmail.com>
2025-05-22 10:56:46 -0300

    melhorando o classifier

--- utils/classifier.py
+++ utils/classifier.py
@@ -1,20 +1,15 @@
 from transformers import pipeline
 
-_model = None  # cache para o modelo carregado
+_model = None
 
 def classify_email(text):
     global _model
     try:
-        if _model is None:
-            _model = pipeline(
-                "text-classification",
-                model="sshleifer/tiny-distilroberta-base"
-            )
-
+        if not _model:
+            _model = pipeline("text-classification", model="sshleifer/tiny-distilroberta-base")
         result = _model(text)[0]
         label = result['label']
         return "Produtivo" if label == "LABEL_1" else "Improdutivo"
-
     except Exception as e:
         print("Erro ao classificar:", str(e))
         return "Improdutivo"
