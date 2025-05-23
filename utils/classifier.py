c4de6dca7a8a8fa12b4310a00952f972d2f6fbe1
Mauro César Guimaraes <113718493+Maurocesar12@users.noreply.github.com>
2025-05-23 00:18:05 -0300

    Update classifier.py
    Modelo diferente da IA

--- utils/classifier.py
+++ utils/classifier.py
@@ -1,15 +1,8 @@
 from transformers import pipeline
 
-_model = None
+# Novo modelo mais preciso
+classifier = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 def classify_email(text):
-    global _model
-    try:
-        if not _model:
-            _model = pipeline("text-classification", model="sshleifer/tiny-distilroberta-base")
-        result = _model(text)[0]
-        label = result['label']
-        return "Produtivo" if label == "LABEL_1" else "Improdutivo"
-    except Exception as e:
-        print("Erro ao classificar:", str(e))
-        return "Improdutivo"
+    result = classifier(text)[0]['label']
+    return "Produtivo" if result == "POSITIVE" else "Improdutivo"
